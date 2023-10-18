619ce05fd246ed5bb93391f6cbb05f191e7a427e
ayubdekow <145904872+ayubdekow@users.noreply.github.com>
2023-10-18 22:45:22 +0300

    Add files via upload

---
+++ in boolean.c..cpp
@@ -0,0 +1,15 @@
+#include <Stdio.h>
+
+int main(void)
+{
+    int a;
+printf("Enter a number:");
+    scanf("%d", &a);
+    if ( a )
+    {
+        printf("The value is True\n");
+
+    }
+    return 0;
+}
+
